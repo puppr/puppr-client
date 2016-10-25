21d58528da5103bf7e99f050d7e9d555dbac61b4
ksavage97 <ksavage97@gmail.com>
2016-10-24 19:22:49 -0700

    edit profile

--- src/app/profile/edit.profile.controller.js
+++ src/app/profile/edit.profile.controller.js
@@ -5,12 +5,16 @@
         .module('app')
         .controller('EditController', EditController);
 
-    EditController.$inject = ['ownerFactory', 'breedFactory', 'petPhotoFactory', 'petFactory', '$ngBootbox', '$stateParams', 'authFactory'];
+    EditController.$inject = ['ownerFactory', 'breedFactory', 'petPhotoFactory', '$mdDialog', 'petFactory', '$ngBootbox', '$stateParams', 'authFactory', '$scope'];
 
-    function EditController(ownerFactory, breedFactory, petPhotoFactory, petFactory, $ngBootbox, $stateParams, authFactory) {
+    function EditController(ownerFactory, breedFactory, petPhotoFactory, $mdDialog, petFactory, $ngBootbox, $stateParams, authFactory, $scope) {
         var vm = this;
         vm.currentOwnerId = authFactory.ownerId;
         vm.onFileUploaded = onFileUploaded;
+
+  //       $scope.myInterval = 3000;
+  // $scope.noWrapSlides = false;
+  // $scope.active = 0;
 
 
         function activate(id) {
@@ -41,23 +45,37 @@
 
         activateTwo();
 
-        
+
 
         function onFileUploaded(file, petId) {
-            petPhotoFactory.addPhoto({ 
-                petId: petId, url: file.url 
-            }).then(
-                function(photo) {
-                    vm.photo = photo;
-                    var p = 0;
-                    for (p = 0; p < vm.owner.pets.length; p++) {
-                        if (vm.owner.pets[p].petId === petId) {
-                            console.log(vm.owner.pets.length);
-                            vm.owner.pets[p].petPhotos.push(vm.photo);
+            var a = 0;
+            for (a = 0; a < vm.owner.pets.length; a++) {
+                if (vm.owner.pets[a].petId === petId && vm.owner.pets[a].petPhotos.length >= 4) {
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                        .clickOutsideToClose(true)
+                        .title('Sorry for the inconvenience')
+                        .textContent("You may only upload a maximum of four photos per dog.")
+                        .ok('Got it!')
+                    );
+                } else {
+                    petPhotoFactory.addPhoto({
+                        petId: petId,
+                        url: file.url
+                    }).then(
+                        function(photo) {
+                            vm.photo = photo;
+                            var p = 0;
+                            for (p = 0; p < vm.owner.pets.length; p++) {
+                                if (vm.owner.pets[p].petId === petId) {
+                                    console.log(vm.owner.pets.length);
+                                    vm.owner.pets[p].petPhotos.push(vm.photo);
+                                }
+                            }
                         }
-                    }
+                    );
                 }
-            );
+            }
         }
 
         vm.editPet = function(pet) {
@@ -71,7 +89,7 @@
             );
 
         };
- 
+
 
     }
 
